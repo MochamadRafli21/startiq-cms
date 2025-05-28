5ba5fddd40990cfadcf25c84e6be48607e03037c
Mochamad Rafli <raflitea123@gmail.com>
2025-05-28 23:52:00 +0700

    chore: auth setup

---
+++ drizzle/0000_fair_boomerang.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `users` (
+	`id` int AUTO_INCREMENT NOT NULL,
+	`name` varchar(255),
+	`email` varchar(255) NOT NULL,
+	`password` varchar(255),
+	CONSTRAINT `users_id` PRIMARY KEY(`id`)
+);
