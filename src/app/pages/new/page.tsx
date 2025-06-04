4a903daf1c330a94b2ef21caa6baa6bf23db9b5b
Mochamad Rafli <raflitea123@gmail.com>
2025-06-04 23:09:10 +0700

    chore: minimize and expand info on new page

--- src/app/pages/new/page.tsx
+++ src/app/pages/new/page.tsx
@@ -13,12 +13,19 @@ const PageEditor = dynamic(
 import Link from "next/link";
 import PageInfo from "@/components/organisms/pages/pages-info";
 import { Button } from "@/components/ui/button";
-import { Save, ChevronsLeft, PanelsTopLeft } from "lucide-react";
+import {
+  Save,
+  ChevronsLeft,
+  PanelsTopLeft,
+  SquareChevronDown,
+  SquareChevronUp,
+} from "lucide-react";
 
 import { useRouter } from "next/navigation";
 import { Page } from "@/types/page.type";
 
 export default function NewPage() {
+  const [minimizeInfo, setMinimizeInfo] = useState<any>(null);
   const [saving, setSaving] = useState(false);
   const [slugError, setSlugError] = useState("");
   const [pageData, setPageData] = useState<Page>({
@@ -106,17 +113,43 @@ export default function NewPage() {
 
   return (
     <div className="flex w-full">
-      <div className="flex flex-col">
-        <div className="flex gap-2 font-semibold items-center p-4 border-b border-gray-200">
-          <Link href="/admin">
-            <Button size="icon">
-              <ChevronsLeft />
-            </Button>
-          </Link>
-          <PanelsTopLeft />
-          <h6>Page Editor</h6>
+      <div
+        className={
+          minimizeInfo ? "absolute flex flex-col" : "relative flex flex-col"
+        }
+      >
+        <div className="flex flex-col border-b border-gray-200">
+          <div className="flex items-center gap-2 font-semibold p-4">
+            <Link href="/admin">
+              <Button size="sm">
+                <ChevronsLeft size="sm" />
+              </Button>
+            </Link>
+            <PanelsTopLeft />
+            <h6>Page Editor</h6>
+          </div>
+
+          <Button
+            hidden={!minimizeInfo}
+            size="sm"
+            className="absolute bottom-[-24] rounded-none z-[1000] w-full py-1 px-1 flex flex-row items-center justify-center"
+            onClick={() => setMinimizeInfo(false)}
+          >
+            <SquareChevronDown /> Expand Info
+          </Button>
         </div>
-        <PageInfo page={pageData} onChange={handleInfoChange} />
+        <div className="flex flex-col" hidden={minimizeInfo}>
+          <PageInfo page={pageData} onChange={handleInfoChange} />
+          <div className="w-full">
+            <Button
+              variant="ghost"
+              className="w-full py-3 px-2 flex flex-row items-center justify-center"
+              onClick={() => setMinimizeInfo(true)}
+            >
+              <SquareChevronUp /> Minimize Info
+            </Button>
+          </div>
+        </div>
       </div>
       <div className="flex flex-col w-full">
         <div className="flex justify-end font-semibold p-3 border-b border-l border-gray-200">
@@ -125,10 +158,18 @@ export default function NewPage() {
             Save
           </Button>
         </div>
-        <PageEditor
-          content={pageData.content}
-          onContentChange={handleContentChange}
-        />
+        <div
+          className={
+            minimizeInfo
+              ? "w-full min-h-screen hover:absolute hover:z-[1001] hover:bottom-[-48]"
+              : "w-full min-h-screen"
+          }
+        >
+          <PageEditor
+            content={pageData.content}
+            onContentChange={handleContentChange}
+          />
+        </div>
       </div>
       {saving && <LoadingPage isLoading={saving} />}
     </div>
