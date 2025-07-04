ec4a14ea6f4b462902a95bfcfe9656314cac05c7
Mochamad Rafli <raflitea123@gmail.com>
2025-07-04 09:01:40 +0700

    chore: update seeder

--- src/db/seeder/content.ts
+++ src/db/seeder/content.ts
@@ -1,13 +1,16 @@
 import { db } from "@/db/client";
-import { pages, templates } from "@/db/schema";
+import { pages, templates, links } from "@/db/schema";
 import fs from "fs/promises";
 import path from "path";
 import type { PageBodyInput } from "@/types/page.type";
 import type { Template } from "@/types/template.type";
+import type { Link } from "@/types/link.type";
+
+type ContentTypes = PageBodyInput | Template | Link;
 
 async function seedFromFolder(
   relativeFolderPath: string,
-  table: typeof pages | typeof templates,
+  table: typeof pages | typeof templates | typeof links,
 ) {
   const absolutePath = path.resolve(__dirname, relativeFolderPath);
   const files = await fs.readdir(absolutePath);
@@ -18,15 +21,25 @@ async function seedFromFolder(
     const filePath = path.join(absolutePath, file);
     const raw = await fs.readFile(filePath, "utf-8");
     const data = JSON.parse(raw);
-
-    const title =
-      "title" in data ? (data as PageBodyInput | Template).title : file;
-
-    try {
-      await db.insert(table).values(data);
-      console.log(`✅ Seeded: ${title}`);
-    } catch (err) {
-      console.error(`❌ Failed to seed ${title}:`, err);
+    if (Array.isArray(data)) {
+      data.forEach(async (innerData) => {
+        const title =
+          "title" in innerData ? (innerData as ContentTypes).title : file;
+        try {
+          await db.insert(table).values(innerData);
+          console.log(`✅ Seeded: ${title}`);
+        } catch (err) {
+          console.error(`❌ Failed to seed ${title}:`, err);
+        }
+      });
+    } else {
+      const title = "title" in data ? (data as ContentTypes).title : file;
+      try {
+        await db.insert(table).values(data);
+        console.log(`✅ Seeded: ${title}`);
+      } catch (err) {
+        console.error(`❌ Failed to seed ${title}:`, err);
+      }
     }
   }
 }
@@ -37,6 +50,9 @@ async function seed() {
 
   console.log("🌱 Seeding pages...");
   await seedFromFolder("data/pages", pages);
+
+  console.log("🌱 Seeding links...");
+  await seedFromFolder("data/links", links);
 }
 
 seed().catch((err) => {
